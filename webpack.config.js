2b10a1d5845cc560b7ad038f6a85a16e826ce6e2
Andrew Beaven <andrew@simplydating.com>
2021-01-29 13:34:34 +1300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+﻿const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (argv) => {
+  return {
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.less$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'less-loader'
+          ]
+        },
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+    ]
+  };
+};
