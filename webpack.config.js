dfda8668c551695e76686515667aaa622f4fdfce
Andrew Beaven <andrew@simplydating.com>
2021-01-29 13:50:33 +1300

    Condense

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,20 @@
 ﻿const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = (argv) => {
-  return {
-    mode: 'production',
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader'
-          ]
-        },
-        {
-          test: /\.less$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'less-loader'
-          ]
-        },
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin(),
+module.exports = () => ({
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(less|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader'
+        ]
+      },
     ]
-  };
-};
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ]
+});
